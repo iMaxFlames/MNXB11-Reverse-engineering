d5ebd3bbac9d299d93b417fd7ffb3bacb7ae10f6
Ayush Chakraborty <chak.polly37@gmail.com>
2023-10-28 20:13:38 +0200

    Completed step 4 and changed the types of certain variables

--- code/main.cxx
+++ code/main.cxx
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <string>
 
-auto genenrate_key(auto key, auto argument_1) {
+auto genenrate_key(std::string argument_1) {
+  auto key{0};
   for (auto element : argument_1) {
     key += element;
   }
@@ -9,19 +10,25 @@ auto genenrate_key(auto key, auto argument_1) {
   return key;
 }
 
-int main(int number_of_arguments, char *arguments[]) {
-  if (number_of_arguments == 3) {
-    std::string program_name{arguments[0]};
-    auto argument_1_first_character{*(arguments[1])};
-    auto program_name_length{program_name.size()};
-    auto argument_2{std::atoi(arguments[2])};
-    auto key{0};
-    std::string argument_1{arguments[1]};
-    key = genenrate_key(key, argument_1);
-    if ((key ^ argument_1_first_character * 3) << (program_name_length & 0x1f) == argument_2) {
+void comparing_key(std::string program_name, std::string argument_1, char argument_1_first_character, int argument_2) {
+  auto program_name_length{program_name.size()};
+  auto key{genenrate_key(argument_1)};
+  
+  if ((key ^ argument_1_first_character * 3) << (program_name_length & 0x1f) == argument_2) {
       std::cout << "Correct!" << std::endl;
     } else {
       std::cout << "Wrong!" << std::endl;
     }
+}
+
+int main(int number_of_arguments, char *arguments[]) {
+  if (number_of_arguments == 3) {
+    std::string program_name{arguments[0]};
+    char argument_1_first_character{*(arguments[1])};
+    std::string argument_1{arguments[1]};
+    int argument_2{std::atoi(arguments[2])};
+
+    comparing_key(program_name, argument_1, argument_1_first_character, argument_2);
+    
   }
 }
