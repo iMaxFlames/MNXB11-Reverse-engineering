acc31bd9658f44ab8270a07007713f2ad3eb85c2
Ayush Chakraborty <chak.polly37@gmail.com>
2023-10-28 20:40:27 +0200

    Fixed main.cxx from step 4

--- code/main.cxx
+++ code/main.cxx
@@ -1,34 +1,36 @@
 #include <iostream>
 #include <string>
 
-auto genenrate_key(std::string argument_1) {
-  auto key{0};
+auto checksum_function(std::string argument_1) {
+  auto checksum{0};
   for (auto element : argument_1) {
-    key += element;
+    checksum += element;
   }
 
-  return key;
+  return checksum;
 }
 
-void comparing_key(std::string program_name, std::string argument_1, char argument_1_first_character, int argument_2) {
+auto calculate_key(std::string program_name, std::string argument_1, char argument_1_first_character) {
   auto program_name_length{program_name.size()};
-  auto key{genenrate_key(argument_1)};
-  
-  if ((key ^ argument_1_first_character * 3) << (program_name_length & 0x1f) == argument_2) {
-      std::cout << "Correct!" << std::endl;
-    } else {
-      std::cout << "Wrong!" << std::endl;
-    }
+  auto checksum{checksum_function(argument_1)};
+
+  return (checksum ^ argument_1_first_character * 3) << (program_name_length & 0x1f);
 }
 
 int main(int number_of_arguments, char *arguments[]) {
   if (number_of_arguments == 3) {
     std::string program_name{arguments[0]};
-    char argument_1_first_character{*(arguments[1])};
     std::string argument_1{arguments[1]};
     int argument_2{std::atoi(arguments[2])};
-
-    comparing_key(program_name, argument_1, argument_1_first_character, argument_2);
+    char argument_1_first_character{*(arguments[1])};
     
+    
+    
+    auto key{calculate_key(program_name, argument_1, argument_1_first_character)};
+    if (key == argument_2) {
+      std::cout << "Correct!" << std::endl;
+    } else {
+      std::cout << "Wrong!" << std::endl;
+    }
   }
 }
