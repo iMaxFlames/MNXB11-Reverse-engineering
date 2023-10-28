9a110cb1007a2470d6e7425aa51c29dae17bf9cc
Ayush Chakraborty <chak.polly37@gmail.com>
2023-10-28 19:17:01 +0200

    Step 3: factoring out the while-loop. Factored out thewhile-loop into a separate function, which for now is called func1.

--- code/main.cxx
+++ code/main.cxx
@@ -1,5 +1,17 @@
 #include <iostream>
 #include <string>
+
+auto func1(auto var6, auto argument_1) {
+  auto var7{0};
+  while (true) {
+      var6 += argument_1[var7++];
+      if (var7 >= static_cast<int>(argument_1.size())) {
+        break;
+      }
+    }
+    return var6;
+}
+
 int main(int number_of_arguments, char *arguments[]) {
   if (number_of_arguments == 3) {
     std::string program_name{arguments[0]};
@@ -7,14 +19,8 @@ int main(int number_of_arguments, char *arguments[]) {
     auto program_name_length{program_name.size()};
     auto argument_2{std::atoi(arguments[2])};
     auto var6{0};
-    auto var7{0};
     std::string argument_1{arguments[1]};
-    while (true) {
-      var6 += argument_1[var7++];
-      if (var7 >= static_cast<int>(argument_1.size())) {
-        break;
-      }
-    }
+    
     if ((var6 ^ argument_1_first_character * 3) << (program_name_length & 0x1f) == argument_2) {
       std::cout << "Correct!" << std::endl;
     } else {
