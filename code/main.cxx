a4281a574463bb3b1ce5dbf959749d64720270a9
Ayush Chakraborty <chak.polly37@gmail.com>
2023-10-28 19:34:49 +0200

    Finshed step 3: factoring out the while-loop.

--- code/main.cxx
+++ code/main.cxx
@@ -1,12 +1,12 @@
 #include <iostream>
 #include <string>
 
-auto func1(auto var6, auto argument_1) {
+auto genenrate_key(auto key, auto argument_1) {
   for (auto element : argument_1) {
-    var6 += element;
+    key += element;
   }
 
-  return var6;
+  return key;
 }
 
 int main(int number_of_arguments, char *arguments[]) {
@@ -15,10 +15,10 @@ int main(int number_of_arguments, char *arguments[]) {
     auto argument_1_first_character{*(arguments[1])};
     auto program_name_length{program_name.size()};
     auto argument_2{std::atoi(arguments[2])};
-    auto var6{0};
+    auto key{0};
     std::string argument_1{arguments[1]};
-    
-    if ((var6 ^ argument_1_first_character * 3) << (program_name_length & 0x1f) == argument_2) {
+    key = genenrate_key(key, argument_1);
+    if ((key ^ argument_1_first_character * 3) << (program_name_length & 0x1f) == argument_2) {
       std::cout << "Correct!" << std::endl;
     } else {
       std::cout << "Wrong!" << std::endl;
