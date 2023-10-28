4bb10f461893d50116f6780bcbe12247956fb0fc
Ayush Chakraborty <chak.polly37@gmail.com>
2023-10-28 19:00:32 +0200

    Completed step 3: renaming variables

--- code/main.cxx
+++ code/main.cxx
@@ -1,21 +1,21 @@
 #include <iostream>
 #include <string>
-int main(int var0, char *var1[]) {
-  if (var0 == 3) {
-    std::string var2{var1[0]};
-    auto var3{*(var1[1])};
-    auto var4{var2.size()};
-    auto var5{std::atoi(var1[2])};
+int main(int number_of_arguments, char *arguments[]) {
+  if (number_of_arguments == 3) {
+    std::string program_name{arguments[0]};
+    auto argument_1_first_character{*(arguments[1])};
+    auto program_name_length{program_name.size()};
+    auto argument_2{std::atoi(arguments[2])};
     auto var6{0};
     auto var7{0};
-    std::string var8{var1[1]};
+    std::string argument_1{arguments[1]};
     while (true) {
-      var6 += var8[var7++];
-      if (var7 >= static_cast<int>(var8.size())) {
+      var6 += argument_1[var7++];
+      if (var7 >= static_cast<int>(argument_1.size())) {
         break;
       }
     }
-    if ((var6 ^ var3 * 3) << (var4 & 0x1f) == var5) {
+    if ((var6 ^ argument_1_first_character * 3) << (program_name_length & 0x1f) == argument_2) {
       std::cout << "Correct!" << std::endl;
     } else {
       std::cout << "Wrong!" << std::endl;
